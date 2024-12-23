17fc0ca55321aef89b9fe56f29682fdfc93d668e
Thomas Cheek <148750626+DeviousFate@users.noreply.github.com>
2024-12-22 19:11:51 -0600

    Add files via upload

---
+++ 7-1_FinalProjectMilestones/Source/ViewManager.h
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////
+// viewmanager.h
+// ============
+// manage the viewing of 3D objects within the viewport
+//
+//  AUTHOR: Brian Battersby - SNHU Instructor / Computer Science
+//	Created for CS-330-Computational Graphics and Visualization, Nov. 1st, 2023
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "ShaderManager.h"
+#include "camera.h"
+
+// GLFW library
+#include "GLFW/glfw3.h" 
+
+class ViewManager
+{
+public:
+	// constructor
+	ViewManager(ShaderManager* pShaderManager);
+	// destructor
+	~ViewManager();
+
+	// mouse position callback for mouse interaction with the 3D scene
+	static void Mouse_Position_Callback(GLFWwindow* window, double xMousePos, double yMousePos);
+
+	// Scroll callback for mouse wheel interaction
+	static void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+
+private:
+	ShaderManager* m_pShaderManager; 	// pointer to shader manager object
+	GLFWwindow* m_pWindow; 	// active OpenGL display window
+
+	// process keyboard events for interaction with the 3D scene
+	void ProcessKeyboardEvents();
+
+public:
+	// create the initial OpenGL display window
+	GLFWwindow* CreateDisplayWindow(const char* windowTitle);
+	
+	// prepare the conversion from 3D object display to 2D scene display
+	void PrepareSceneView();
+};
